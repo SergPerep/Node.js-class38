e8d01e7dee0cab3b3fc60e2cb4b37b2fa9fbe1d6
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-08-06 23:46:57 +0200

    Finish prep-exercises

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -2,13 +2,44 @@
  * Exercise 3: Create an HTTP web server
  */
 
-const http = require('http');
+const http = require("http");
+const { readFile } = require("fs");
+
+const PORT = process.env.PORT || 3000;
 
 //create a server
-let server = http.createServer(function (req, res) {
+let server = http.createServer((req, res) => {
   // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  switch (req.url) {
+    case "/":
+      readFile("./index.html", (err, buf) => {
+        if (err) return console.error(err);
+        const htmlStr = buf.toString();
+        res.setHeader("Content-Type", "text/html");
+        res.write(htmlStr);
+        res.end();
+      });
+      break;
+    case "/index.js":
+      readFile("./index.js", (err, buf) => {
+        if (err) return console.error(err);
+        res.setHeader("Content-Type", "text/plain");
+        const jsStr = buf.toString();
+        res.write(jsStr);
+        res.end();
+      });
+			break;
+		case "/style.css":
+			readFile("./style.css", (err, buf) => {
+				if (err) return console.error(err);
+				res.setHeader("Content-Type", "text/css");
+				const cssStr = buf.toString();
+				res.write(cssStr);
+				res.end();
+			})
+  }
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); // The server starts to listen on port 3000
