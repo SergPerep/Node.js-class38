0022c4bfe1925fc0ffb7ace948e506e514ca7159
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-08-12 14:17:40 +0200

    Connect PORT to env variable and show message when server is running

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express();
- 
+
+const PORT = process.env.PORT || 3000;
 
 // YOUR CODE GOES IN HERE
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+})
