0bf50f808f4668ff9703c7d53b6a59ed9c902893
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-08-12 15:48:01 +0200

    Design a route for updating an existing post: PUT /posts/:title

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -17,10 +17,11 @@ app.post("/blogs", (req, res) => {
   try {
     const { title, content } = req.body;
     const pathToFile = path.join(__dirname, `/blogs/${title}.txt`);
-    if (fs.existsSync(pathToFile)) return res.status(400).send({
-      status: 400,
-      error: "File already exists"
-    });
+    if (fs.existsSync(pathToFile))
+      return res.status(400).send({
+        status: 400,
+        error: "File already exists",
+      });
     fs.writeFileSync(pathToFile, content);
     res.send({
       status: 200,
@@ -33,6 +34,21 @@ app.post("/blogs", (req, res) => {
       error: "Internal server error",
     });
   }
+});
+
+app.put("/posts/:title", (req, res) => {
+  const title = req.params.title;
+  const { content } = req.body;
+  // What if the request does not have a title and/or content?
+  const pathToFile = path.join(__dirname, `/blogs/${title}.txt`);
+  console.log(pathToFile);
+  if (!fs.existsSync(pathToFile))
+    return res.send({
+      status: 400,
+      error: "This post does not exist!",
+    });
+  fs.writeFileSync(pathToFile, content);
+  res.end("ok");
 });
 
 app.listen(PORT, () => {
