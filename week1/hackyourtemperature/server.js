322088b50d229d92a57d54ed6e1ba4237c22c02d
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-08-15 17:10:29 +0200

    Add custom request logger middleware

--- week1/hackyourtemperature/server.js
+++ week1/hackyourtemperature/server.js
@@ -5,6 +5,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const logRequests = (req, res, next) => {
+  console.log("\x1b[32m%s\x1b[0m", `-- ${req.method} ${req.path}`);
+  next();
+}
+
+app.use(logRequests);
+
 app.get("/", (req, res) => {
   try {
     res.send("Hello from backend to frontend!");
