40b04203764581e4525a3af69b100a17541f670c
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-08-15 17:03:26 +0200

    Dublicate hackyourtemperature of week1 branch to week2 branch

---
+++ week1/hackyourtemperature/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  try {
+    res.send("Hello from backend to frontend!");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.post("/weather", (req, res) => {
+  try {
+    const { cityName } = req.body;
+    res.send(cityName);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`-> Server is running on port ${PORT}`);
+});
